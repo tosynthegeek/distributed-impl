9fdf1e35bd278c47433ecfc8d3345fd4ff8a587d
tosynthegeek <oluwatosinojedapo@gmail.com>
2025-07-30 23:21:16 +0100

    feat: build protos

--- raft/build.rs
+++ raft/build.rs
@@ -1,7 +1,10 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure().compile_protos(
-        &["proto/client_service.proto", "proto/internal_service.proto"],
-        &["proto/"],
-    )?;
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(true)
+        .compile_protos(
+            &["proto/internal_service.proto", "proto/client_service.proto"],
+            &["proto"],
+        )?;
     Ok(())
 }
